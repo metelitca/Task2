00b794ce5bdacfad77b1c800a094513271ec7ee2
metelitca <35520726+metelitca@users.noreply.github.com>
2025-08-15 10:03:13 +0300

    Задание 2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double variable1 = 125845.75426;
+        byte variable2 = 127;
+
+        System.out.printf("Переменная с плавающей точкой: %f%n", variable1);
+        System.out.printf("Целочисленная переменная: %d%n", variable2);
+    }
+}
